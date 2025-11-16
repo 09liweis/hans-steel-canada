4c3bab887c6546bee133807dc2e21f2fdb79dbde
SiriYeung <119325339+SiriYeung@users.noreply.github.com>
2025-11-16 17:26:27 +0000

    add image and write about information

---
+++ constants/projectsData.ts
@@ -0,0 +1,230 @@
+export const projectsData = [
+  {
+    id: 'woodside-square',
+    title:'Woodside Square',
+    features:[
+      'Function: Mall + Cinema Complex',
+      'Location: Scarborough',
+      'GFA: 108,091 ft2 (20,042 m2)',
+      'Height: 25m',
+      'Steel Tonnage: 1,800 ton'
+    ],
+    mainImage: "/images/projects-new/woodside_square/01.png",
+    images:[
+      "/images/projects-new/woodside_square/02.png",
+      "/images/projects-new/woodside_square/03.png",
+    ]
+  },
+
+  {
+    id: 'mount_dennis_station',
+    title:'mount_dennis_station',
+    features:[
+      'Function: Crosstown LRT Main Station',
+      'Location: Toronto',
+      'Work: Historical Building Movement and Renovation + New Main Bus Terminal ',
+      'Steel Tonnage: 600 ton'
+    ],
+    mainImage: "/images/projects-new/mount_dennis_station/01.png",
+    images:[
+      "/images/projects-new/mount_dennis_station/02.png",
+      "/images/projects-new/mount_dennis_station/03.png",
+    ]
+  },
+
+  {
+    id: 'orillia_rec_centre',
+    title:'orillia_rec_centre',
+    features:[
+      'Function: Community Recreation Centre',
+      'Location: Orillia',
+      'GFA: 136,794 ft2 (12,708.6 m2)',
+      'Height: 12m',
+      'Steel Tonnage: 900 ton'
+    ],
+    mainImage: "/images/projects-new/orillia_rec_centre/01.png",
+    images:[
+      "/images/projects-new/orillia_rec_centre/02.png",
+      "/images/projects-new/orillia_rec_centre/03.png",
+    ]
+  },
+
+  {
+    id: 'cranberry_restore',
+    title:'cranberry_restore',
+    features:[
+      'Function: Retirement Home',
+      'Location: Collingwood',
+      'GFA: 72,787 ft2 (6762 m2)',
+      'Height: 15m',
+      'Steel Tonnage: 300 ton'
+    ],
+    mainImage: "/images/projects-new/cranberry_restore/01.png",
+    images:[
+      "/images/projects-new/cranberry_restore/02.png",
+      "/images/projects-new/cranberry_restore/03.png",
+    ]
+  },
+
+  {
+    id: 'richmond_hill_cemetery',
+    title:'richmond_hill_cemetery',
+    features:[
+      'Function: Community Cemetery',
+      'Location: Richmond Hill',
+      'GFA: 8105 ft2 (753 m2)',
+      'Height: 10m',
+      'Steel Tonnage: 150 ton'
+    ],
+    mainImage: "/images/projects-new/richmond_hill_cemetery/01.png",
+    images:[
+      "/images/projects-new/richmond_hill_cemetery/02.png",
+      "/images/projects-new/richmond_hill_cemetery/03.png",
+    ]
+  },
+    
+  {
+    id: 'canada_royal_milk_inc.',
+    title:'canada_royal_milk_inc.',
+    features:[
+      'Function: Milk Formula Facility',
+      'Location: Toronto',
+      'GFA: 1,661,409 ft2 (154,350 m2)',
+      'Height: 35m',
+      'Steel Tonnage: 800 ton'
+    ],
+    mainImage: "/images/projects-new/canada_royal_milk_inc./01.png",
+    images:[
+      "/images/projects-new/canada_royal_milk_inc./02.png",
+      "/images/projects-new/canada_royal_milk_inc./03.png",
+    ]
+  },
+
+  {
+    id: 'maple_leaf_poultry',
+    title:'maple_leaf_poultry',
+    features:[
+      'Function: Poultry Shop',
+      'Location: London',
+    ],
+    mainImage: "/images/projects-new/maple_leaf_poultry/01.png",
+    images:[
+      "/images/projects-new/maple_leaf_poultry/02.png",
+      "/images/projects-new/maple_leaf_poultry/03.png",
+      "/images/projects-new/maple_leaf_poultry/04.png",
+      "/images/projects-new/maple_leaf_poultry/05.png",
+    ]
+  },
+
+  {
+    id: 'westbridge_warehouse',
+    title:'westbridge_warehouse',
+    features:[
+      'Function: Storage Warehouse',
+      'Location: Toronto',
+      'GFA: 324,708 ft2 (30,166.3 m2)',
+      'Height: 12m',
+      'Steel Tonnage: 385 ton'
+    ],
+    mainImage: "/images/projects-new/westbridge_warehouse/01.png",
+    images:[
+      "/images/projects-new/westbridge_warehouse/02.png",
+    ]
+  },
+
+
+  {
+    id: 'gilbert_storage',
+    title:'gilbert_storage',
+    features:[
+      'Function: Storage',
+      'Location: Toronto',
+      'GFA: 176.664 ft2 (2,833.3 m2)',
+      'Height: 22.38m',
+      'Steel Tonnage: 385 ton'
+    ],
+    mainImage: "/images/projects-new/gilbert_storage/01.png",
+    images:[
+      "/images/projects-new/gilbert_storage/02.png",
+    ]
+  },
+
+  {
+    id: 'mining_stone_crusher',
+    title:'mining_stone_crusher',
+    features:[
+      
+    ],
+    mainImage: "/images/projects-new/mining_stone_crusher/01.png",
+    images:[
+      "/images/projects-new/mining_stone_crusher/02.png",
+      "/images/projects-new/mining_stone_crusher/03.png",
+      "/images/projects-new/mining_stone_crusher/04.png",
+    ]
+  },
+
+  {
+    id: 'mining_production_modular',
+    title:'mining_production_modular',
+    features:[
+      
+    ],
+    mainImage: "/images/projects-new/mining_production_modular/01.png",
+    images:[
+      "/images/projects-new/mining_production_modular/02.png",
+    ]
+  },
+
+  {
+    id: 'mining_production_modular',
+    title:'mining_production_modular',
+    features:[
+      
+    ],
+    mainImage: "/images/projects-new/mining_production_modular/03.png",
+    images:[
+      "/images/projects-new/mining_production_modular/04.png",
+      "/images/projects-new/mining_production_modular/05.png",
+    ]
+  },
+
+  {
+    id: 'clamsheel_dreger',
+    title:'clamsheel_dreger',
+    features:[
+      
+    ],
+    mainImage: "/images/projects-new/clamsheel_dreger/01.png",
+    images:[
+      "/images/projects-new/clamsheel_dreger/02.png",
+      "/images/projects-new/clamsheel_dreger/03.png",
+    ]
+  },
+
+  {
+    id: 'transmission_tower',
+    title:'transmission_tower',
+    features:[
+      
+    ],
+    mainImage: "/images/projects-new/transmission_tower/01.png",
+    images:[
+      "/images/projects-new/transmission_tower/02.png",
+      "/images/projects-new/transmission_tower/03.png",
+      "/images/projects-new/transmission_tower/04.png",
+    ]
+  },
+
+  {
+    id: 'transmission_tower',
+    title:'transmission_tower',
+    features:[
+      
+    ],
+    mainImage: "/images/projects-new/transmission_tower/04.png",
+    images:[
+      "/images/projects-new/transmission_tower/05.png",
+      "/images/projects-new/transmission_tower/06.png",
+    ]
+  },
+]
