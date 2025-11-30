c4d1f67fa75f24070ced89a86fdc89895511e7ff
09liweis <weisen.li@hotmail.com>
2025-11-30 09:29:16 -0500

    add constant company name

--- constants/text.ts
+++ constants/text.ts
@@ -1,6 +1,6 @@
 export const CONTACT_PHONE = "+19056401000";
 const CONTACT_EMAIL = "";
-const COMPANY_NAME = "";
+export const COMPANY_NAME = "Hans Steel Canada";
 export const COMPANY_ADDRESS = "6 Sangster Road, Uxbridge, ON, L9P 0G5";
 
 // Phone number formatting functions
