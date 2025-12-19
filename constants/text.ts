f0c5eeccc75eb470927adbb4cc16437a93387208
SiriYeung <119325339+SiriYeung@users.noreply.github.com>
2025-12-19 22:02:33 +0000

    export contact email

--- constants/text.ts
+++ constants/text.ts
@@ -1,5 +1,5 @@
 export const CONTACT_PHONE = "+19056401000";
-const CONTACT_EMAIL = "";
+export const CONTACT_EMAIL = "";
 export const COMPANY_NAME = "Hans Steel Canada";
 export const COMPANY_ADDRESS = "6 Sangster Road, Uxbridge, ON, L9P 0G5";
 
